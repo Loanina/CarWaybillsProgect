2090de7eeb6f58141a24b625629c6eacab64f269
Loanina <yhololeenko@mail.ru>
2023-10-12 15:52:15 +0300

    Init

---
+++ CarWaybills/Pages/WaybillsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarWaybills
+{
+    /// <summary>
+    /// Логика взаимодействия для WaybillsPage.xaml
+    /// </summary>
+    public partial class WaybillsPage : Page
+    {
+        public WaybillsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarWaybills/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CarWaybills.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private Page carsPage;
+        private Page waybillsPage;
+
+        private Page currentPage;
+        public Page CurrentPage { 
+            get { return currentPage; }
+            set { 
+                currentPage = value;
+                RaisePropertyChanged(() => CurrentPage);
+        }   }
+
+        private double frameOpacity;
+        public double FrameOpacity { get { return frameOpacity; } set { frameOpacity = value; RaisePropertyChanged(() => CurrentPage); } }
+
+
+        public MainViewModel()
+        {
+            carsPage = new Page();
+            waybillsPage = new Page();
+
+            FrameOpacity = 1;
+            CurrentPage = carsPage;
+        }
+
+        public ICommand bMenuCarsPage_Click
+        {
+            get
+            {
+                return new RelayCommand(() => SlowOpacity(carsPage));
+            }
+        }
+
+        public ICommand bMenuWaybillsPage_Click
+        {
+            get
+            {
+                return new RelayCommand(() => SlowOpacity(waybillsPage));
+            }
+        }
+
+        private async void SlowOpacity(Page page)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                for (double i = 1.0; i>0.0; i -= 0.1)
+                {
+                    FrameOpacity = i;
+                    Thread.Sleep(50);
+                }
+                CurrentPage = page;
+                for (double i =0.0; i< 1.0; i += 0.1)
+                {
+                    FrameOpacity = i;
+                    Thread.Sleep(50);
+                }
+            });
+        }
+    }
+}
